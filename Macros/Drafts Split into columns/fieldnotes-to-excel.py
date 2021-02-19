6282b2aedb1a977794ae807ae23e9286af2e895e
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-19 19:55:40 +0000

    Script

--- Macros/Drafts Split into columns/fieldnotes-to-excel.py
+++ Macros/Drafts Split into columns/fieldnotes-to-excel.py
@@ -7,28 +7,41 @@ import shutil
 
 def search ():
 
-    current_pwd = os.getcwd()
+    pwd = os.getcwd()
     field_note = "E:\Garmin\geocache_visits.txt" # --> "F:same"
 
     try:
         shutil.copy(field_note, pwd)
-        return export (pwd)
+        return export ()
 
     except NotADirectoryError:
         error = "This file cannot be found"
         return error
 
 
-def export (directory):
+def export ():
+    
+    import xlwt
+    import xlrd
+    from datetime import datetime
 
-    file = os.rename("geocache_visits.txt", "drafts.txt")
-
+    current_day = datetime.now()
+    date = current_day.strftime("%Y-%m-%d")
+    sheet = xlwt.Workbook()
+    Excel = sheet.add_sheet(date)
+    os.rename("geocache_visits.txt", "drafts.txt")
     drafts = open("drafts.txt", "r+") # Read and paste
 
+    DATA = drafts.readlines()
+    for i in range(len(DATA)):
+        row = DATA[i].split(",") 
+
+        for j in range(len(row)):
+            Excel.write(i, j, row[j])
+
+    sheet.save("Logs " + date + ".xls")
+    drafts.close()
 
 
-    
-
-
-if __name__ = "__main__":
+if __name__ == "__main__":
     search ()
