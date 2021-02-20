968719e88b3f66ebd5c7096894c0511dacf83ca8
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-20 21:34:09 +0000

    Delete trash and complete C++ script

--- Macros/Coordinates Between Caches/AreaPoints.cpp
+++ Macros/Coordinates Between Caches/AreaPoints.cpp
@@ -2,23 +2,54 @@
 
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <string>
+using namespace std;
 
-void Points_to_area (int points) {
+// Functions
 
-    double area
+void Points_to_area (double points) {
 
-    std::cout << "Area: " << area << std::endl;
+    int area;
+    area = ceil(pow((sqrt(points)-1),2)*(pow(1.39,2)));
+    cout << "Area: " << area << endl;
 }
 
-void Points_to_area (int points) {
+void Area_to_points (double area) {
 
-    double area
-    
-    std::cout << "Area: " << area << std::endl;
+    int points;
+    points = floor(pow((((sqrt(area))/1.39)+1),2));
+    cout << "Number of points: " << points << endl;
 }
 
 int main () 
 {
-    
+    string answer;
+    double qtd;
+    vector<string> inputs = {
+                                "Points", 
+                                "Area",
+                            } ;
+    answer = "something";
+
+    do  {
+            cout << "Would you like to ask for the number of points or available area? Points/Area: ";
+            cin >> answer;
+
+            if (answer != inputs[0] && answer != inputs[1]) {
+                cout << "Input error. Please try again." << endl;
+            }
+
+            else { 
+                cout << "Number: ";
+                cin >> qtd;
+                if (answer == "Points") {
+                    Area_to_points (qtd);
+                }
+                else {
+                    Points_to_area (qtd);
+                }
+            }
+        }   while (answer != inputs[0] && answer != inputs[1]);
     return 0;
 }
