de38cfac369d8ce121df586dddeb35f51fb024e1
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-20 20:49:15 +0000

    Fini

--- Macros/Coordinates Between Caches/AreaPoints.py
+++ Macros/Coordinates Between Caches/AreaPoints.py
@@ -6,35 +6,45 @@ from math import sqrt, pow
 
 def Points_to_Area (points):
 
+    area = round((pow((sqrt(points)-1),2))*pow(1.39,2),2)
+    print ("Available area: {}".format(area))
+    return "Finished"
 
-    return area
 
 def Area_to_Points (area):
 
+    number_of_points = round(pow(((sqrt(area)/1.39)+1),2))
+    print ("Number of points: {}".format(number_of_points))
+    return "Finished"
 
-
-    return number_of_points
 
 def search ():
 
     available_inputs =  {
-                            "Points" : "available"
-                            "Area" : "available"
+                            "Points" : "available",
+                            "Area" : "available",
                         }
 
     answer = "something"
 
-    while ( answer not in [ key.lower() for key, status in available_inputs.items() 
+    while ( answer.lower() not in [ key.lower() for key, status in available_inputs.items() 
                             if available_inputs[key] == "available" ] ) :
 
-        answer = str(input("")).strip().lower()
+        answer = str(input("Would you like to ask for the number of points or available area? Points/Area: ")).strip().upper()
 
-        if answer == "POINTS".lower():
+        if answer == "AREA":
+
+            data = int(input("Number of points: "))
             return Points_to_Area (data)
-        elif answer == ""
+
+        elif answer == "POINTS":
+
+            data = int(input("Area: "))
             return Area_to_Points (data)
+            
         else:
             return "Input error. Please try again."
+
 
 if __name__ == "__main__":
     search ()
