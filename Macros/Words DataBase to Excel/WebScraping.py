f392064f7881de48c921ef133a99049d7b301361
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-21 17:35:25 +0000

    Webscraping to excel

--- Macros/Words DataBase to Excel/WebScraping.py
+++ Macros/Words DataBase to Excel/WebScraping.py
@@ -1,1 +1,30 @@
-init
+# Modules
+
+from bs4 import BeautifulSoup
+import math
+import os
+import shutil
+import requests
+
+# Functions
+
+def do_request (link):
+
+    request = requests.get(link)
+    soup = BeautifulSoup(request, 'lxml')
+    important = soup.find_all('p').get_text()
+    print(important)
+
+
+def get_text ():
+    return content
+
+
+def to_excel (content):
+    return None
+
+# Beginning
+
+url = "https://pt.wikipedia.org/wiki/Geocaching"
+if __name__ == "__main__":
+    do_request (url)
