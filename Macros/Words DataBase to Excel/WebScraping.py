b1293fea11e7f5d533d10c209e037684b5d1d3d0
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-21 20:02:08 +0000

    Scraping

--- Macros/Words DataBase to Excel/WebScraping.py
+++ Macros/Words DataBase to Excel/WebScraping.py
@@ -1,10 +1,12 @@
 # Modules
 
 from bs4 import BeautifulSoup
-import math
-import os
-import shutil
 import requests
+from os import getcwd as pwd
+from string import punctuation, ascii_letters
+import xlsxwriter
+from xlsxwriter import Workbook
+from datetime import datetime
 
 # Functions
 
@@ -17,24 +19,23 @@ def do_request (link, alphabetic_orderphabetic_order):
 def get_text (html, alphabetic_order):
     
     def normalize (lines):
-        
-        from string import punctuation, ascii_letters
+    
         numbers = [str(x) for x in range(10)]
-        
         norm = []
         trash_words =  [
                         'a', 'as', 'e', 'é', 'o', 'os', 'de', 'por', 'para', 'um', 'uma', 'uns',
                         'tal', 'em', 'mas', 'como', 'no', 'na', 'ou', 'and', 'or', 'the', 'se',
                         'nesta', 'porém', 'esta', 'da', 'do', 'n', 'w', 'ac', 'dc', 'ad', 'este',
-                        '','','','','','','','','','','','','','',
+                        'nas', 'tem', 'are', 'inc', 'if', 'more', 'us', 'd', 'for', 'be','das',
+                        'dos', 'com', 'in', 'pela', 'pelo', 'pelas', 'pelos', 'isto', 'aquilo',
                        ]
             
         for line in lines:
             
             # Remove all punctuation and paragraphs
-            line = line.replace("\n", "").replace("–", "")
+            line = line.replace("\n", "").replace("–", " ").replace("\r", " ")
             for char in line:
-                if char in punctuation:
+                if char in punctuation.replace("-", ""):
                     line = line.replace(char, "") 
             
             # Filter word by word
@@ -81,19 +82,40 @@ def get_text (html, alphabetic_order):
 
 def to_excel (content):
     
-    from datetime import datetime
-    
+    current_directory = pwd()
     current_day = datetime.now()
     date = current_day.strftime(" %Y-%m-%d")
     title = "DataBase " + content[0].capitalize() + date
-    print(title)
+
+    workbook = xlsxwriter.Workbook(title + ".xlsx")
+    Excel = workbook.add_worksheet(date)
+      
+    y = 0
+    x = 0
+    Excel.write(y, x, "Attemps")
+    for header in [x for x in range(1, 11)]:
+        Excel.write(y+1, x, header)
+        x += 1
+       
+    x = 0
+    y = 2
+    for word in content:
+        print("")
+    
+    workbook.close()
+
+
+def bomb_checker ():
+        
+    # In future, this function will be able to get words in current worksheet
+    # and push 10 words automatically every 10 minutes
     
     return None
 
 
 # Beginning
 
-url = "https://pt.wikipedia.org/wiki/Portugal"
+url = "https://www.geocaching.com/geocache/GC8FKWV_quinto-imperio"
 alphabetic_order = False
 
 if __name__ == "__main__":
