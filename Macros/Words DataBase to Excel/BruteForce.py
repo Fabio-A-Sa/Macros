49e52391ec7d4e4fdf572435752b446f6a82e555
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-22 13:06:04 +0000

    Adicionado esboço

---
+++ Macros/Words DataBase to Excel/BruteForce.py
@@ -0,0 +1,30 @@
+# Modules
+
+import os
+import shutil
+import requests
+import xlsxwriter
+from selenium import WebDriver
+from bs4 import BeautifulSoup
+from xlsxwriter import Workbook
+from datetime import datetime
+
+# Functions
+
+def get_data ():
+    return None
+
+
+def brute_force():
+
+    data = get_data()
+    return data
+
+
+
+
+url = "something.com"
+pwd = os.getcwd()
+
+if __name__ == "__main__":
+    brute_force()
