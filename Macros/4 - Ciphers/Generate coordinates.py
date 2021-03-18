d547010797ba2720b089f4719c85ae26fa6014fc
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-18 16:54:09 +0000

    Changes

--- Macros/4 - Ciphers/Generate coordinates.py
+++ Macros/4 - Ciphers/Generate coordinates.py
@@ -6,12 +6,19 @@ print(coordinates.default())
 
 def generate():
 
+    attemps = []
+
+
     for attem in attemps:
         print(attemp)
-        
+
     return attemps
 
 def start():
 
+    global header1, header2
     answer = str().lower().strip()
     if answer == 
+
+if __name__ == "__main__":
+    star()
