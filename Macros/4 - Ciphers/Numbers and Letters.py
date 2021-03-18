dfbc022a84cf25430ca9a0d2b8ba6966612b74cb
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-18 15:44:29 +0000

    more

--- Macros/4 - Ciphers/Numbers and Letters.py
+++ Macros/4 - Ciphers/Numbers and Letters.py
@@ -1,31 +1,33 @@
-# Modules
-
-from string import ascii_letters as abc
-numbers = [x for x in range(0, 10)]
-
-# Functions
+from string import ascii_lowercase as abc
+numbers = [str(x) for x in range(0, 10)]
+global offset
 
 def numbers_to_letters (message) :
 
+    solution = ""
+    for character in message.lower():
+        solution + " "
+        
+    return solution
 
 def letters_to_numbers (message) :
+    
+    return None
 
-
-message = str(input("Your message: ")).split().lower()
-if message[0] in abc:
-    print(letters_to_numbers(message))
-else 
 
 
 def start():
 
-    message = str(input("Your message: ")).split().lower()
-if message[0] in abc:
-    print(letters_to_numbers(message))
-else 
+    message = str(input("Your message: ")).strip().lower()
+    try:
+        offset = int(input("A = "))
+    except ValueError:
+        offset = 0
+    finally:
+        if message[0] in abc:
+            print(letters_to_numbers(message))
+        else:
+            print(numbers_to_letters(message))
 
-
-    return None
-
-if __name__ = "__main__":
+if __name__ == "__main__":
     start()
