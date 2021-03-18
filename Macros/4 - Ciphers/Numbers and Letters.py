278edae62d23b6ff3468f8ac60fbed24ed90e435
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-18 15:10:20 +0000

    Add code

---
+++ Macros/4 - Ciphers/Numbers and Letters.py
@@ -0,0 +1,31 @@
+# Modules
+
+from string import ascii_letters as abc
+numbers = [x for x in range(0, 10)]
+
+# Functions
+
+def numbers_to_letters (message) :
+
+
+def letters_to_numbers (message) :
+
+
+message = str(input("Your message: ")).split().lower()
+if message[0] in abc:
+    print(letters_to_numbers(message))
+else 
+
+
+def start():
+
+    message = str(input("Your message: ")).split().lower()
+if message[0] in abc:
+    print(letters_to_numbers(message))
+else 
+
+
+    return None
+
+if __name__ = "__main__":
+    start()
