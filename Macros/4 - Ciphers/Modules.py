83ee7c87022e123f48b9e9dd423a2daddb169357
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-11 18:31:26 +0100

    Add files

--- Macros/4 - Ciphers/Modules.py
+++ Macros/4 - Ciphers/Modules.py
@@ -17,3 +17,48 @@ class ciphers:
 
     def binary():
         print("soon")
+
+    def nato():
+        def to_nato(words):
+    
+            dictionary = {
+                            'A' : 'Alfa' ,
+                            'B' : 'Bravo' ,
+                            'C' : 'Charlie' ,
+                            'D' : 'Delta' ,
+                            'E' : 'Echo' ,
+                            'F' : 'Foxtrot' ,
+                            'G' : 'Golf' ,
+                            'H' : 'Hotel' ,
+                            'I' : 'India' ,
+                            'J' : 'Juliett' ,
+                            'K' : 'Kilo' ,
+                            'L' : 'Lima' ,
+                            'M' : 'Mike' ,
+                            'N' : 'November' ,
+                            'O' : 'Oscar' ,
+                            'P' : 'Papa' ,
+                            'Q' : 'Quebec' ,
+                            'R' : 'Romeo' ,
+                            'S' : 'Sierra' ,
+                            'T' : 'Tango' ,
+                            'U' : 'Uniform' ,
+                            'V' : 'Victor' ,
+                            'W' : 'Whiskey' ,
+                            'X' : 'Xray' ,
+                            'Y' : 'Yankee' ,
+                            'Z' : 'Zulu' ,
+                        }
+            
+            ponctuation = ['.', '?', '!']
+            solution = ""
+
+            for char in words:
+                if char.upper() in dictionary.keys():
+                    solution += dictionary[char.upper()] + " "
+                elif char in ponctuation:
+                    solution += char + " "
+                else:
+                    continue
+            
+            return solution.strip()
